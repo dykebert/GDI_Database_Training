1403b5c55764455c6b0a7d815ddf8a23e3419f45
dykebert <sharky@pacific-ocean.com>
2015-05-18 06:14:14 -0600

    Initial files for GDI Database Training

---
+++ Items.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Items (
+  ItemID INTEGER PRIMARY KEY,
+  Item VARCHAR(100)
+);
+
+INSERT INTO Items (Item) VALUES ('Clothes');
+INSERT INTO Items (Item) VALUES ('Jackets');
+INSERT INTO Items (Item) VALUES ('Hangers');
+INSERT INTO Items (Item) VALUES ('Towels');
+INSERT INTO Items (Item) VALUES ('Dishes');
+INSERT INTO Items (Item) VALUES ('Pots & Pans');
+INSERT INTO Items (Item) VALUES ('Utensils');
+INSERT INTO Items (Item) VALUES ('Silverware');
+INSERT INTO Items (Item) VALUES ('Toiletries');
+INSERT INTO Items (Item) VALUES ('Shoes');
+INSERT INTO Items (Item) VALUES ('Books');
+INSERT INTO Items (Item) VALUES ('Games');
+INSERT INTO Items (Item) VALUES ('Lawn Mower');
---
+++ Rooms.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Rooms (
+  RoomID INTEGER PRIMARY KEY,
+  Room VARCHAR(100)
+);
+
+
+INSERT INTO Rooms (Room) VALUES ('Kitchen');
+INSERT INTO Rooms (Room) VALUES (2,'Garage');
+INSERT INTO Rooms (Room) VALUES (3,'Family Room');
+INSERT INTO Rooms (Room) VALUES (4,'Dining Room');
+INSERT INTO Rooms (Room) VALUES (5,'Bedroom');
+INSERT INTO Rooms (Room) VALUES (6,'Bathroom');
+INSERT INTO Rooms (Room) VALUES (7,'Closet');
+INSERT INTO Rooms (Room) VALUES (8,'Patio');
